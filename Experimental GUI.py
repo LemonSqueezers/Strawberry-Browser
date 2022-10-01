4e724ca79e6a08ba7aff3bf2f56becf072202823
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2022-10-01 21:56:02 +0530

    Create Experimental GUI.py

---
+++ Experimental GUI.py
@@ -0,0 +1,14 @@
+import requests
+
+import sys
+
+import tkinter as tk
+tk.Tk(screenName=None,  baseName=None,  className='StrawberryGUI',  useTk=1);
+
+
+
+win=tk.Tk() #creating the main window and storing the window object in 'win'
+win.geometry('500x500') #setting the size of the window
+entry = tk.Entry(fg="black", bg="white", width=50)
+data = entry.get
+
