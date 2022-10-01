1e5999a8d8b45a1d0bdbd065e1ab4872abccd9b4
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2022-10-01 17:36:22 +0530

    didnt prefix stuff correctly

--- pyback.py
+++ pyback.py
@@ -1,6 +1,6 @@
 import requests
 print("Strawberry Backend System 0.2 Beta \n")
 import sys
-r = requests.get(sys.argv[1])
+r = requests.get("http://" + sys.argv[1])
 
-print("http://" + r.text)
+print(r.text)
